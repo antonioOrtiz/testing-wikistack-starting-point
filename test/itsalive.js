0c01d6a48933ab4aff6fd062a50fe5488287649f
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2017-08-08 17:21:42 -0400

    wills code

--- test/itsalive.js
+++ test/itsalive.js
@@ -6,27 +6,36 @@ describe('Testing suite capabilities', function () {
   });
 });
 
-// describe('+', function () {
-//   var posNum, negNum;
-//   beforeEach(function () {
-//     posNum = 100;
-//     negNum = -50;
-//   });
-//   it('sums two positive numbers', function () {
-//     var result = posNum + posNum;
-//     expect(result).to.equal(200);
-//   });
-//   it('sums two negative number', function () {
-//     var result = negNum + negNum;
-//     expect(result).to.equal(-100);
-//   });
-//   it('sums a negative and a positive', function () {
-//     var result = negNum + posNum;
-//     expect(result).to.equal(50);
-//   });
-//   it('is commutative', function () {
-//     var resultA = negNum + posNum,
-//       resultB = posNum + negNum;
-//     expect(resultA).to.equal(resultB);
-//   });
-// });
+it('confirms setTimeout\'s timer accuracy', function (done) {
+  var start = new Date();
+  setTimeout(function () {
+    var duration = new Date() - start;
+    expect(duration).to.be.closeTo(1000, 50);
+    done();
+  }, 1000);
+});
+
+describe('+', function () {
+  var posNum, negNum;
+  beforeEach(function () {
+    posNum = 100;
+    negNum = -50;
+  });
+  it('sums two positive numbers', function () {
+    var result = posNum + posNum;
+    expect(result).to.equal(200);
+  });
+  it('sums two negative number', function () {
+    var result = negNum + negNum;
+    expect(result).to.equal(-100);
+  });
+  it('sums a negative and a positive', function () {
+    var result = negNum + posNum;
+    expect(result).to.equal(50);
+  });
+  it('is commutative', function () {
+    var resultA = negNum + posNum,
+      resultB = posNum + negNum;
+    expect(resultA).to.equal(resultB);
+  });
+});
---
+++ test/page.model.spec.js
@@ -0,0 +1,57 @@
+var expect = require('chai').expect;
+var Page = require('../models').Page;
+
+describe('Page model', function () {
+
+  describe('Virtuals', function () {
+      var page;
+      beforeEach(function () {
+      page = Page.build();
+      });
+
+      // Is this dynamic/robust?
+    describe('route', function () {
+      it('returns the url_name prepended by "/wiki/"',
+        function() {
+          page.urlTitle = 'testTitle';
+          expect(page.route).to.equal("/wiki/testTitle");
+        });
+    });
+
+      // What is markdown?
+      // Comparing non-primitive values?
+    describe('renderedContent', function () {
+      it('converts the markdown-formatted content into HTML',
+       function() {
+         page.content = '#### h4 Heading';
+         expect(page.renderedContent).to.equal('<h4>Heading</h4>');
+       });
+    });
+  });
+
+  describe('Class methods', function () {
+    describe('findByTag', function () {
+      it('gets pages with the search tag');
+      it('does not get pages without the search tag');
+    });
+  });
+
+  describe('Instance methods', function () {
+    describe('findSimilar', function () {
+      it('never gets itself');
+      it('gets other pages with any common tags');
+      it('does not get other pages without any common tags');
+    });
+  });
+
+  describe('Validations', function () {
+    it('errors without title');
+    it('errors without content');
+    it('errors given an invalid status');
+  });
+
+  describe('Hooks', function () {
+    it('it sets urlTitle based on title before validating');
+  });
+
+});
